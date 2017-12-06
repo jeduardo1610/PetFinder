537a4ec81a814bc9b803d1064b7067eb708b676d
Jorge Eduardo <jeduardo1601@gmail.com>
2017-12-05 19:59:06 -0600

    Customizing Slider

--- Pet Finder/Theme.swift
+++ Pet Finder/Theme.swift
@@ -85,13 +85,21 @@ enum Theme : Int{
     UISegmentedControl.appearance()
      .setBackgroundImage(selectedControlBackground, for: .selected, barMetrics: .default)
     
-    //UIStepper +/- control
+    //UIStepper +/- control Config
     UIStepper.appearance().setBackgroundImage(controlBackground, for: .normal)
     UIStepper.appearance().setBackgroundImage(controlBackground, for: .disabled)
     UIStepper.appearance().setBackgroundImage(controlBackground, for: .highlighted)
     
     UIStepper.appearance().setDecrementImage(UIImage(named: "fewerPaws"), for: .normal)
     UIStepper.appearance().setIncrementImage(UIImage(named: "morePaws"), for: .normal)
+    
+    //Slider Config
+    UISlider.appearance().setThumbImage(UIImage(named: "sliderThumb"), for: .normal)
+    UISlider.appearance().setMaximumTrackImage(UIImage(named: "maximumTrack")?
+      .resizableImage(withCapInsets: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 6)), for: .normal)
+    UISlider.appearance().setMinimumTrackImage(UIImage(named: "minimumTrack")?
+      .withRenderingMode(.alwaysTemplate)
+      .resizableImage(withCapInsets: UIEdgeInsets(top: 0, left : 6, bottom: 0, right : 0)), for: .normal)
 
   }
   
